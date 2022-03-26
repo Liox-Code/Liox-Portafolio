20b3e6969b24c8c1dcedda96838b9c865ee5ae41
Angel Leonardo Mendieta Castillo <leonardo.mendieta.programer@gmail.com>
2022-03-26 19:47:32 -0400

    Se renombro carpeta de aboutMe a AboutMe

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // Components
-import AboutMe from '../components/aboutMe'
+import AboutMe from '../components/AboutMe'
 import Projects from '../components/Projects'
 
 // Styles
