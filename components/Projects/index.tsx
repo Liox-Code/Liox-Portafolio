6927904872b0f1f719084abebd955d39dc13115c
Liox <Leonardo.mendieta.programer@gmail.com>
2022-08-01 22:49:20 -0400

    Links in contact informatino and in projects items

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -10,24 +10,41 @@ const Projects = () => {
   const ProjectsData = [
     {
       id: 'pd-1',
-      imageLink: '/images/wpMagenta.jpg',
-      title: 'Proyecto 1',
+      imageLink: 'https://dixel.netlify.app/',
+      imagePath: '/images/wpMagenta.jpg',
+      title: 'Dixel - Landing Page',
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta optio amet sint enim quibusdam maiores fuga illo quam aspernatur, qui'
     },
     {
       id: 'pd-2',
-      imageLink: '/images/wpBlue.jpg',
-      title: 'Proyecto 2',
+      imageLink: 'https://landing-page-sauberkeit.netlify.app/',
+      imagePath: '/images/wpBlue.jpg',
+      title: 'Sauberkeit - Landing Page',
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta optio amet sint enim quibusdam maiores fuga illo quam aspernatur, qui'
     },
     {
       id: 'pd-3',
-      imageLink: '/images/wpLightBlue.jpg',
-      title: 'Proyecto 3',
+      imageLink: 'https://dreamy-fermat-56ece1.netlify.app/',
+      imagePath: '/images/wpLightBlue.jpg',
+      title: 'Video Player',
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta optio amet sint enim quibusdam maiores fuga illo quam aspernatur, qui'
+    },
+    {
+      id: 'pd-4',
+      imageLink: 'https://ps-liox.netlify.app/',
+      imagePath: '/images/wpLightBlue.jpg',
+      title: 'Store',
+      description: 'Lorem IPsum'
+    },
+    {
+      id: 'pd-5',
+      imageLink: 'https://instapet-eight.vercel.app/',
+      imagePath: '/images/wpLightBlue.jpg',
+      title: 'Instapet',
+      description: 'Lorem IPsum'
     }
   ]
   return (
--- components/aboutMe/index.tsx
+++ components/aboutMe/index.tsx
@@ -24,16 +24,16 @@ const AboutMe = () => {
         voluptas praesentium in at voluptate.
       </S.Description>
       <S.SocialMediaContainer>
-        <S.SocialMediaIcon>
+        <S.SocialMediaIcon href="https://www.linkedin.com/in/LeonardoMendieta/">
           <S.Icon
             icon={['fab', 'linkedin']}
             iconcolor="var(--linkedin-color)"
           />
         </S.SocialMediaIcon>
-        <S.SocialMediaIcon>
+        <S.SocialMediaIcon href="https://github.com/Liox-Code">
           <S.Icon icon={['fab', 'github']} iconcolor="var(--github-color)" />
         </S.SocialMediaIcon>
-        <S.SocialMediaIcon>
+        <S.SocialMediaIcon href="https://api.whatsapp.com/send?phone=59169772238&text=Hola!%20%F0%9F%98%80%F0%9F%91%8B%F0%9F%A4%9A%F0%9F%91%8B">
           <S.Icon
             icon={['fab', 'whatsapp']}
             iconcolor="var(--whatsapp-color)"
